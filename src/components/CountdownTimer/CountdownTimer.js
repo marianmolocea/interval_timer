8876433701f0b9dc3e3d95089e2923f51ec20126
marianmolocea <marian.mcrt@gmail.com>
2020-12-29 22:50:17 +0000

    foramtted time

--- src/components/CountdownTimer/CountdownTimer.js
+++ src/components/CountdownTimer/CountdownTimer.js
@@ -1,47 +1,31 @@
 import React, { useState, useEffect } from 'react';
 
-const convertHoursToSeconds = (hours) => {
-  return hours * 60 * 60;
-};
-
-const convertMinutesToSeconds = (minutes) => {
-  return minutes * 60;
-};
-
-const calculateTimeLeft = (hours, minutes, seconds) => {
-  let timeLeft = 0;
-  if (hours) {
-    timeLeft += convertHoursToSeconds(hours);
-  }
-  if (minutes) {
-    timeLeft += convertMinutesToSeconds(minutes);
-  }
-  return timeLeft + seconds;
-};
 const CountdownTimer = () => {
-  const [timeLeft, setTimeLeft] = useState(calculateTimeLeft());
-  const [seconds, setSeconds] = useState(10);
-  const [minutes, setMinutes] = useState(0);
-  const [hours, setHours] = useState(1);
+  const [seconds, setSeconds] = useState(5);
+  const [minutes, setMinutes] = useState(1);
+  const [hours, setHours] = useState(0);
 
   useEffect(() => {
     setTimeout(() => {
-      if(seconds) {
-        setSeconds(seconds - 1)
+      if (seconds) {
+        setSeconds(seconds - 1);
       } else if (minutes) {
-        setSeconds(59)
-        setMinutes(minutes - 1)
+        setSeconds(59);
+        setMinutes(minutes - 1);
       } else if (hours) {
-        setSeconds(59)
-        setMinutes(59)
-        setHours(hours - 1)
+        setSeconds(59);
+        setMinutes(59);
+        setHours(hours - 1);
       }
     }, 1000);
   });
 
+  const formatTime = (value) => (value <= 9 ? `0${value}` : value);
   return (
     <div>
-      Hours: {hours} Minutes: {minutes} Seconds: {seconds}
+      {hours ? <span>{formatTime(hours)} : </span> : ''}
+      {<span>{formatTime(minutes)} : </span>}
+      <span>{formatTime(seconds)}</span>
     </div>
   );
 };
