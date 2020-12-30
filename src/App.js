5f82ce9a1f320e5176189019d9a012183a55514a
marianmolocea <marian.mcrt@gmail.com>
2020-12-30 21:22:30 +0000

    Finished the countdown

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      Interval timer
       <MainContainer />
     </div>
   );
--- src/components/CountdownTimer/CountdownTimer.js
+++ src/components/CountdownTimer/CountdownTimer.js
@@ -1,32 +1,96 @@
 import React, { useState, useEffect } from 'react';
+import { Progress, TimePicker } from 'antd';
+import moment from 'moment';
 
-const CountdownTimer = () => {
-  const [seconds, setSeconds] = useState(5);
-  const [minutes, setMinutes] = useState(1);
-  const [hours, setHours] = useState(0);
+import './CountdownTimer.css';
+import 'antd/dist/antd.css';
+
+const totalMilliseconds = (h, m, s, ml = 0) => {
+  const hoursToMlSec = h * 60 * 60 * 1000;
+  const minToMlSec = m * 60 * 1000;
+  const secToMl = s * 1000
+  const totalML = hoursToMlSec + minToMlSec + secToMl + ml
+  return totalML;
+};
+
+const calculatePercentage = (initialTime, timeLeft) =>
+  (timeLeft * 100) / initialTime;
+
+const CountdownTimer = ({ time, start = true, stop }) => {
+  const [percentage, setPercentage] = useState(0);
+  const [deadline, setDeadline] = useState(0);
+  const [timer, setTimer] = useState({hours: 0, minutes: 0, seconds: 0, millisec: 0})
 
   useEffect(() => {
     setTimeout(() => {
-      if (seconds) {
-        setSeconds(seconds - 1);
-      } else if (minutes) {
-        setSeconds(59);
-        setMinutes(minutes - 1);
-      } else if (hours) {
-        setSeconds(59);
-        setMinutes(59);
-        setHours(hours - 1);
+      if(timer.millisec) {
+        setTimer({...timer, millisec: timer.millisec - 25})
+      } else if (timer.seconds) {
+        setTimer({ ...timer, millisec: 1000, seconds: timer.seconds - 1 });
+      } else if (timer.minutes) {
+        setTimer({
+          ...timer,
+          millisec: 1000,
+          seconds: 59,
+          minutes: timer.minutes - 1,
+        });
+      } else if (timer.hours) {
+        setTimer({
+          millisec: 1000,
+          seconds: 59,
+          minutes: 59,
+          hours: timer.hours - 1,
+        });
       }
-    }, 1000);
-  });
+      setPercentage(
+        calculatePercentage(
+          deadline,
+          totalMilliseconds(
+            timer.hours,
+            timer.minutes,
+            timer.seconds,
+            timer.millisec
+          )
+        )
+      );
+    }, 25);
+  }, [deadline, timer]);
 
   const formatTime = (value) => (value <= 9 ? `0${value}` : value);
+
+  const handleInputValue = (value) => {
+    const hr = moment(value).toDate().getHours();
+    const min = moment(value).toDate().getMinutes();
+    const sec = moment(value).toDate().getSeconds();
+    setTimer({
+      seconds: sec,
+      minutes: min,
+      hours: hr,
+      millisec: 0
+    });
+    setDeadline(totalMilliseconds(hr, min, sec));
+  };
+
   return (
-    <div>
-      {hours ? <span>{formatTime(hours)} : </span> : ''}
-      {<span>{formatTime(minutes)} : </span>}
-      <span>{formatTime(seconds)}</span>
-    </div>
+    <Progress
+      type="circle"
+      percent={percentage}
+      width={'70vh'}
+      strokeWidth={3}
+      format={() => {
+        if (!percentage) {
+          return <TimePicker onOk={handleInputValue} />;
+        } else {
+          return (
+            <div style={{color: 'white', fontSize: '32px'}}>
+              {timer.hours ? <span>{formatTime(timer.hours)} : </span> : ''}
+              {<span>{formatTime(timer.minutes)} : </span>}
+              <span>{formatTime(timer.seconds + 1)}</span>
+            </div>
+          );
+        }
+      }}
+    />
   );
 };
 
